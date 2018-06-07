8cbe45a669d75069ca1c9b412313fe824cc22c4c
natsumi.hironaka <natsumi.hironaka@rakuten.com>
2018-06-07 06:16:15 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 });
 
 Route::resource('haikus', 'HaikusController');
+
+// user registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
